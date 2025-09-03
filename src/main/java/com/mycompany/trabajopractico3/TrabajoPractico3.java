c02030142d8fd344ec2ca613392ece131512a39f
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2025-09-03 17:42:00 -0300

    Primer commit

---
+++ src/main/java/com/mycompany/trabajopractico3/TrabajoPractico3.java
@@ -0,0 +1,12 @@
+package com.mycompany.trabajopractico3;
+
+/**
+ *
+ * @author Fabrizio
+ */
+public class TrabajoPractico3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
